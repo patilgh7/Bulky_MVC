fc4aa901c13fe8b6a90e3529810fc8ddcabc0524
Gaurav <gaurav.patil@teople.co.in>
2023-12-20 23:47:50 +0530

    Input Tag Helpers

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,15 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
     public class Category
     {
-        [Key]
+        // Key annotation used for this is Id
+        // Required annotation Specifies that a data field value is required.
+        // DisplayName annotation used to show that name on UI
+
+        [Key]     
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Category Name")]    
         public string Name { get; set; }
 
+        [DisplayName("Display Order")]   
         public int DisplayOrder { get; set; }
 
 
