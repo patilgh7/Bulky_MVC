1c9b85c0f0e5a5c8914f439fea512be5a49394d9
Gaurav <gaurav.patil@teople.co.in>
2023-12-28 19:28:53 +0530

    N-Tier Architecture

---
+++ Bulky.Utility/SD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Utility
+{
+    public static class SD
+    {
+        // SD => Static Details
+        // Here we are mentioning all the constants for our websites in this class
+
+    }
+}
