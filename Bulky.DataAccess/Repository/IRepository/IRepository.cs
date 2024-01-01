20017462622b7b97b9c4106ebf34e52af73317cf
Gaurav <gaurav.patil@teople.co.in>
2024-01-01 17:43:40 +0530

    Repository Pattern (Created IRepository Inteface)

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        // T - Category
+        IEnumerable<T> GetAll();
+
+        // When you want to pass predicate that means condition check in method like Find() method or FirstorDefault() method
+        // For that syntax => Expression<Func<T,bool>> filter
+        T Get(Expression<Func<T,bool>> filter);
+
+        void Add(T entity);
+        void Remove(T entity);
+        
+        void RemoveRange(IEnumerable<T> entities);
+
+       
+    }
+}
