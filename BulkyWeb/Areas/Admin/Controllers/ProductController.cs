8b49418a3984df8013ee42fcb685aa76a9f27d6d
Gaurav <gaurav.patil@teople.co.in>
2024-01-05 14:43:38 +0530

    Combine Create and Edit Pages of Product. Upsert Operation => Update + Insert => It is combination of Create and Edit Functionality

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -25,10 +25,9 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-
-        // Create Operation
-
-        public IActionResult Create()
+       
+        // Upsert Operation => Update + Insert => It is combination of Create and Edit Functionality
+        public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new()
             {
@@ -40,13 +39,24 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 Product = new Product()
             };
 
+            if(id == null || id == 0)
+            {
+                // Create
+                return View(productVM);
+            }
+            else
+            {
+                // Update
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
 
-            return View(productVM);
+           
         }
 
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
          
             if (ModelState.IsValid)
@@ -78,74 +88,6 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             }
 
            
-        }
-
-        //  ---------------------------------xxxxx-------------------------------------------
-
-        // For Edit 
-
-        /*
-         * int? id
-         * id parameter is nullable
-         * when you add question mark after any datatype like int,float or class like Product
-         * int? represents a nullable integer or flaot? represents a nullable float or Product? then it is said to be nullable
-         * nullableInt can hold an integer value or be explicitly set to null.
-         * In summary, a nullable type is a type that can represent both a valid value of its underlying type and the absence of a value (null). 
-         * This feature is particularly helpful when dealing with scenarios where the presence or absence of data needs to be explicitly managed in the code. 
-         * The code checks if the id is null. If it is, it returns a bad request result, 
-         * indicating that the provided identifier is invalid. 
-         * If the id is not null, the method proceeds with further processing.
-         */
-
-        // Update Operation
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            // 3 Approaches for finding the id for editing....but best approach is using FirstOrDefault method
-
-            // Using Find method you can only deal with primary key
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-
-            // Using FirstOrDefault method you can deal with any parameter like Id, Name anything
-            // Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-
-            // Using Where method
-            // Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-
-                //Actual saving the updated data to database which is filled by UI
-                _unitOfWork.Save();
-
-                // Create TempData for display notification where the data updated successfully or not.
-                TempData["success"] = "Product updated successfully";
-
-                // You can pass action only i.e Index when you are same controller otherwise pass 2nd parameter controller is good practice
-                //return RedirectToAction("Index","Product");
-
-                return RedirectToAction("Index");
-            }
-
-            return View();
         }
 
 
