7d2151990df79c5e8f6d70786bf2d780edd83b28
Gaurav <gaurav.patil@teople.co.in>
2024-01-30 19:16:48 +0530

    Created Order Summary UI

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -40,6 +40,11 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
         }
 
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
         public IActionResult Plus(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
