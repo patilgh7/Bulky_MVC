2d4d8667cafc057f362e2b7124b662bc52490328
Gaurav <gaurav.patil@teople.co.in>
2024-02-06 00:00:01 +0530

    When remove the items from cart it will shows changes respectively and when you logged out cart will clear and it will show zero items in cart ....When you logged in it will show cart items value if you have added anything.

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -221,11 +221,14 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 
         public IActionResult Minus(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked:true);
 
             if (cartFromDb.Count <= 1)
             {
                 // remove that from cart
+                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+                    .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
+
                 _unitOfWork.ShoppingCart.Remove(cartFromDb);
             }
             else
@@ -243,7 +246,10 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 
         public IActionResult Remove(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked:true);
+
+            HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+                .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
 
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
 
